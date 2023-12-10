ee75225f6693b1866c171f8dc10fb83d5d845ae8
jenna5376 <jenna5376@gmail.com>
2023-12-10 03:22:40 -0500

    made site responsive

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -5,7 +5,7 @@ import logo from '../assets/logo.svg'
 import logoDark from '../assets/logo-dark.svg'
 import links from '../data/links'
 import LinkCard from '../components/LinkCard'
-import { LightModeOutlined, DarkModeOutlined } from '@mui/icons-material';
+import { LightModeOutlined, DarkModeOutlined, Menu } from '@mui/icons-material';
 
 function SideNav() {
     const getCurrentTheme = () => window.matchMedia("(prefers-color-scheme: dark)").matches;
@@ -19,43 +19,58 @@ function SideNav() {
     })
     const addBodyClass = className => document.body.classList.add(className);
     const removeBodyClass = className => document.body.classList.remove(className);
-
+    
     useEffect(() => {
         localStorage.setItem('theme', isDark ? 'dark' : 'light')
         if (isDark) addBodyClass('dark')
         else removeBodyClass('dark')
-    }, [isDark])
+}, [isDark])
+
+    const [showMenu, setShowMenu] = useState(false)
 
     return (
         <nav className='side-nav'>
-            <img className='side-nav__logo' src={isDark ? logoDark : logo}/>
+            <div className='side-nav__top'>
+                <img className='side-nav__logo' src={isDark ? logoDark : logo}/>
+                <div className='side-nav__icons'>
+                    {isDark ? 
+                        <LightModeOutlined className='theme__icon' onClick={() => setIsDark(!isDark)} /> : 
+                        <DarkModeOutlined className='theme__icon' onClick={() => setIsDark(!isDark)} />
+                    }
+                    <Menu className='theme__icon' onClick={() => {setShowMenu(!showMenu)}} />
+                </div>
+            </div>
             <p className='side-nav__intro'>I'm Jenna, a multidisciplinary developer at NYU. I love creating user-centered designs and bringing them to life with code!</p>
-            {links.slice(0, 2).map((link, i) => {
-                return (
-                    <LinkCard 
-                        key={i}
-                        image={link.image}
-                        title={link.title}
-                        description={link.description}
-                        link={link.link}
-                        external={false}
-                    />
-                )
-            })}
-            <p className='side-nav__section'>External Links</p>
-            {links.slice(3, 8).map((link, i) => {
-                return (
-                    <LinkCard 
-                        key={i}
-                        image={link.image}
-                        title={link.title}
-                        description={link.description}
-                        link={link.link}
-                        external={true}
-                    />
-                )
-            })}
-            <div className='side-nav__theme chip' onClick={() => setIsDark(!isDark)}>
+            <div 
+                className={`side-nav__links ${showMenu ? 'side-nav__links--display' : ''}`}
+            >
+                {links.slice(0, 2).map((link, i) => {
+                    return (
+                        <LinkCard 
+                            key={i}
+                            image={link.image}
+                            title={link.title}
+                            description={link.description}
+                            link={link.link}
+                            external={false}
+                        />
+                    )
+                })}
+                <p className='side-nav__section'>External Links</p>
+                {links.slice(3, 8).map((link, i) => {
+                    return (
+                        <LinkCard 
+                            key={i}
+                            image={link.image}
+                            title={link.title}
+                            description={link.description}
+                            link={link.link}
+                            external={true}
+                        />
+                    )
+                })}
+            </div>
+            <div className='side-nav__theme' onClick={() => setIsDark(!isDark)}>
                 {isDark ? 
                     <LightModeOutlined className='theme__icon' fontSize='small' /> : 
                     <DarkModeOutlined className='theme__icon' fontSize='small' />
